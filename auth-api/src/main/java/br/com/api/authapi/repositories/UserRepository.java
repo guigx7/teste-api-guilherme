af348cd18671a2de0cc04c96ff3b56417a1f9c32
guigx <guigo211103@gmail.com>
2024-05-11 03:39:42 -0300

    Created SecurityFilter

--- auth-api/src/main/java/br/com/api/authapi/repositories/UserRepository.java
+++ auth-api/src/main/java/br/com/api/authapi/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package br.com.api.authapi.repositories;
 import br.com.api.authapi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
