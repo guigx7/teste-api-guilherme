31816528997620af75ba3ec47c18d031867d6619
guigx <guigo211103@gmail.com>
2024-05-11 05:55:34 -0300

    Updated CorsConfig

--- auth-api/src/main/java/br/com/api/authapi/infra/cors/CorsConfig.java
+++ auth-api/src/main/java/br/com/api/authapi/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") //alterar para o domínio que o front vai rodar
+                .allowedOrigins("http://localhost:5173") //alterar para o domínio que o front vai rodar
                 .allowedMethods("GET", "POST");
     }
 }
