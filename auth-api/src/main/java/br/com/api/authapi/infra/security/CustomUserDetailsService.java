4f73b9adcf6d0bcd11f2f4f4a22553f4c84ce4c9
guigx <guigo211103@gmail.com>
2024-05-11 03:49:50 -0300

    Created CustomUserDetailsService

---
+++ auth-api/src/main/java/br/com/api/authapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package br.com.api.authapi.infra.security;
+
+import br.com.api.authapi.domain.user.User;
+import br.com.api.authapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
