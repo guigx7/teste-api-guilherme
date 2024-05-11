2519f35e5416b5159221f37603ae9308bae26253
guigx <guigo211103@gmail.com>
2024-05-11 05:52:35 -0300

    Configurated CORS

---
+++ auth-api/src/main/java/br/com/api/authapi/infra/cors/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.api.authapi.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") //alterar para o domínio que o front vai rodar
+                .allowedMethods("GET", "POST");
+    }
+}
